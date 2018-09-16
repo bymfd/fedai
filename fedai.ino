9c783c1f44eb2d3f97e7daeb5fb927b74445a550
bymfd <frukolosko@gmail.com>
2018-09-16 18:27:35 +0300

    add buzzer sonme bug resolved

--- fedai.ino
+++ fedai.ino
@@ -9,6 +9,7 @@ This repo:https://github.com/bymfd/fedai
  int IN3 = 4; 
 int IN4 = 3;
 int led=13;
+const int buzzer = 5; //sound feedback pins (buzzer)
 #include <SPI.h>
 #include <MFRC522.h>
  
@@ -18,9 +19,10 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
  
 void setup() 
 {
-  pinMode (led, OUTPUT); 
-  pinMode (IN3, OUTPUT);
-pinMode (IN4, OUTPUT);
+  pinMode (led, OUTPUT);//arduino led  
+  pinMode (IN3, OUTPUT);//motor left pin
+pinMode (IN4, OUTPUT);//motor right pin 
+pinMode(buzzer, OUTPUT);
   Serial.begin(9600);   // Initiate a serial communication
   SPI.begin();      // Initiate  SPI bus
   mfrc522.PCD_Init();   // Initiate MFRC522
@@ -54,39 +56,33 @@ void loop()
   Serial.println();
   Serial.print("message: ");
   content.toUpperCase();
-  if (content.substring(1) == "55 9E D9 E2" ) //change here the UID of the card/cards that you want to give access
+  if (content.substring(1) == "55 9E D9 E2" || content.substring(1) == "80 6F DF B0" || content.substring(1) == "4A 41 56 D3" ) //change here the UID of the card/cards that you want to give access
   {
+
+
     Serial.println("Welcome!!!");
+    valids();// card true sound 
     Serial.println();
-    asagi();
-    digitalWrite (IN4, HIGH);
-    Serial.println("Asagi");
-    delay(5000);
-    Serial.println("yukari");
-    digitalWrite (IN4, LOW);
+    mdown();//unlock door 
+    Serial.println("open door");
+    Serial.println("ok!! door relock 5 sec...");
+    delay(5000); //wait 5 seconds to enter the room
+    valids();//warning to lock door
+    mup();//lock door
+   Serial.println("door locked");
     
   }
  
  else   {
     Serial.println("invalid card");
-    delay(500);
-    digitalWrite (IN3, HIGH);
-    delay(500);
-    digitalWrite (IN3, LOW);
-    delay(500);
-    digitalWrite (IN3, HIGH);
-    delay(500);
-    digitalWrite (IN3, LOW);
-    delay(500);
-    digitalWrite (IN3, HIGH);
-    delay(500);
-    digitalWrite (IN3, LOW);
+   invalids();//warning invalid sound
+    
   }
 }
 
 
 
-void yukari()
+void mup()//door unlock 
 {
   digitalWrite (IN4, LOW); 
 digitalWrite (IN3, HIGH);
@@ -98,7 +94,7 @@ digitalWrite (IN3,LOW);
 digitalWrite (IN4,LOW);
 
 } 
-void asagi()
+void mdown() //lock door 
 {
  digitalWrite (IN3,LOW);
 digitalWrite (IN4, HIGH); 
@@ -109,4 +105,19 @@ delay(50);
 digitalWrite (IN4,LOW); 
 
 }
+ void invalids()//invalid card sound if you want change 
+ {
+for(int hz = 440; hz < 1000; hz++){
+    tone(buzzer, hz, 50);
+    delay(5);
+  } }
+    
+   void valids()//valid card sound
+   {
+     tone(buzzer,2000);
+    delay(1000);
+     noTone(buzzer);
+     delay(500);
+     
+   } 
 
